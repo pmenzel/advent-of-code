d753055bc95657fd4a87a73e132ed3546ae8ffd2
Peter Menzel <pmenzel@gmail.com>
2025-12-06 21:47:03 +0100

    2025 day6 part2

---
+++ 2025/day6/part2.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl -w
+use strict;
+use warnings;
+
+sub transpose {
+  my ($m) = @_;            # $m is arrayref of rows
+  my $rows = @$m;
+  my $cols = @{$m->[0]};
+  my @t;
+  for my $c (0 .. $cols-1) {
+    for my $r (0 .. $rows-1) {
+        $t[$c][$r] = $m->[$r][$c];
+    }
+  }
+  return \@t;              # arrayref
+}
+
+# read input into 2D array
+my @m;
+while (<>) {
+  next unless length;
+  chomp;
+  my @F = split //;
+  next unless @F;
+  push @m, \@F;
+}
+
+my @m2 = @{ transpose(\@m) };
+
+my @numbers;
+my $sum;
+for(my $i=$#m2; $i >=0; $i--) {
+	my $n = join "", @{ $m2[$i] } ;
+	if($n =~ m/(.*)([+*])$/) {
+	  push @numbers, $1;
+	  $sum += eval(join($2,@numbers));
+	  @numbers = ();
+	}
+	else {
+		next if $n =~ /^\s+$/;
+	  push @numbers, $n;
+	}
+}
+print "sum=$sum\n";
+
